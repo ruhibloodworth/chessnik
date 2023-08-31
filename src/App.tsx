399dfb1ef0aac154b19b1cbef48c90081e16232b
Ruhi Bloodworth <ruhi.bloodworth@gmail.com>
2023-08-31 11:43:43 -0500

    scaffold project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Chessboard } from "react-chessboard";
+
+export default function App() {
+  return (
+    <>
+      <div style={{ height: "75vh", width: "75vh" }}>
+        <Chessboard />
+      </div>
+    </>
+  );
+}
